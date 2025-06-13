83dfcbfb2fccbcbc284d916f0267d967b7a74c3f
MJatto <33674005+MJatto@users.noreply.github.com>
2025-06-13 07:19:58 -0500

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,8 +1,9 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
   cluster_name    = "java-eks-cluster"
   cluster_version = "1.29"
-  subnets         = module.vpc.private_subnets
+  subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
   eks_managed_node_groups = {
